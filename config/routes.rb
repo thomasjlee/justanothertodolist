5872fd2d4d56e2d178b1e6491bf7edde027adec2
Thomas Lee <leethomasjae@gmail.com>
2018-11-30 07:59:11 -0600

    Clear Completed button works with JS disabled.

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   root to: "todo_lists#index"
   resources :todo_lists do
     resources :todo_items, only: [:create, :update, :destroy] do
+      collection do
+        delete :clear_completed, to: "todo_items#clear_completed", as: "clear"
+      end
       member do
         patch :complete
-      end
-      collection do
-        post :clear_completed, to: "todo_items#clear_completed", as: "clear"
       end
     end
   end
