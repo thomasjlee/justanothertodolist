d1622ab19ab57574aab7b4ee62ca9a6e0ce65e93
Thomas Lee <leethomasjae@gmail.com>
2018-12-01 10:12:38 -0600

    Update text and border colors of Clear Completed button.

--- app/assets/javascripts/jata.js
+++ app/assets/javascripts/jata.js
@@ -8,10 +8,12 @@ document.addEventListener('turbolinks:load', function() {
 function toggleClearCompletedButton() {
   if (document.querySelector('[data-clearable]')) {
     jata.clearCompletedButton.removeAttribute('disabled');
-    jata.clearCompletedButton.classList.add('clickable');
+    jata.clearCompletedButton.classList.add('clickable', 'text-info', 'border-info');
+    jata.clearCompletedButton.classList.remove('text-secondary', 'border-secondary');
   } else {
     jata.clearCompletedButton.setAttribute('disabled', true);
-    jata.clearCompletedButton.classList.remove('clickable');
+    jata.clearCompletedButton.classList.add('text-secondary', 'border-secondary');
+    jata.clearCompletedButton.classList.remove('clickable', 'text-info', 'border-info');
   }
 }
 
