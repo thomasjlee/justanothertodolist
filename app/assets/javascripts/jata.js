ab5a4ae810f9b255ea25cf96d6f2d26c5649b63a
Thomas Lee <leethomasjae@gmail.com>
2018-12-02 10:47:32 -0600

    Clean up edit todo functionality.

--- app/assets/javascripts/jata.js
+++ app/assets/javascripts/jata.js
@@ -1,8 +1,19 @@
 document.addEventListener('turbolinks:load', function() {
   window.jata = {
-    clearCompletedButton: document.querySelector('form[action$="clear_completed"] input[type=submit]'),
-    toggleClearCompletedButton: toggleClearCompletedButton,
+    init: function() {
+      this.attachEditHandlers();
+    },
+    clearCompletedButton:
+      document.querySelector('form[action$="clear_completed"] input[type=submit]'),
+    toggleClearCompletedButton : toggleClearCompletedButton,
+    attachEditHandlers         : attachEditHandlers,
+    hideEditForms              : hideEditForms,
+    displayTodoTexts           : displayTodoTexts,
+    onEditClicked              : onEditClicked,
+    toggleViewState            : toggleViewState
   }
+
+  jata.init();
 });
 
 function toggleClearCompletedButton() {
@@ -17,3 +28,45 @@ function toggleClearCompletedButton() {
   }
 }
 
+function onEditClicked(event) {
+  const todo = event.target.closest('.todo-item');
+  const editForm = todo.querySelector('.edit-todo-form');
+  const todoText = todo.querySelector('.todo-text');
+  if (editForm.style.display === 'block') {
+    jata.toggleViewState();
+  } else {
+    jata.toggleViewState();
+    editForm.style.display = 'block';
+    todoText.style.display = 'none';
+  }
+}
+
+function attachEditHandlers() {
+  for (let editButton of document.getElementsByClassName('edit-btn')) {
+    editButton.addEventListener('click', jata.onEditClicked);
+  }
+}
+
+function attachNewEditHandler(todoId) {
+  const todo = document.getElementById(todoId);
+  todo.addEventListener('click', jata.onEditClicked);
+}
+
+function displayTodoTexts() {
+  const todoTexts = document.querySelectorAll('.todo-text');
+  for (let todoText of todoTexts) {
+    todoText.style.display = 'block';
+  }
+}
+
+function hideEditForms() {
+  const editForms = document.querySelectorAll('.edit-todo-form');
+  for (let editForm of editForms) {
+    editForm.style.display = 'none';
+  }
+}
+
+function toggleViewState() {
+  jata.displayTodoTexts();
+  jata.hideEditForms();
+}
