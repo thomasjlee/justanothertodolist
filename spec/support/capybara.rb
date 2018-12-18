28e97eb719be5664c96a7d360ed6b7e1dcc319f7
Thomas Lee <leethomasjae@gmail.com>
2018-12-18 08:03:40 -0600

    Adjust capybara config. Add travis chrome addon.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,14 +1,11 @@
 RSpec.configure do |config|
-  config.before(:all, type: :system) do
-    Capybara.server = :puma, { Silent: true }
-  end
-
   config.before(:each, type: :system) do
     driven_by :rack_test
   end
 
   config.before(:each, type: :system, js: true) do
     driven_by :selenium_chrome_headless
+    Capybara.server = :puma, { Silent: true }
   end
 end
 
