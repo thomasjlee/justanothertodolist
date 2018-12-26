1b8492e17e01626f72cdcf7c0dbf91304148ab92
Thomas Lee <leethomasjae@gmail.com>
2018-12-26 07:39:37 -0600

    Move stimulus textarea controller to textarea element. Replace with helper.

---
+++ app/helpers/stimulus_helper.rb
@@ -0,0 +1,7 @@
+module StimulusHelper
+  def stim_textarea
+    { controller: "textarea",
+      action:     "change->textarea#resize input->textarea#resize",
+      target:     "textarea.textarea" }
+  end
+end
