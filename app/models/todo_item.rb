ecc56a17e78e55ac0722028e9b8ef1af44933f3a
Thomas Lee <leethomasjae@gmail.com>
2018-12-29 10:37:19 -0600

    Gracefully degrade edit todo item textarea to row size 3. Remove TodoItem#textarea_rows.

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -3,10 +3,6 @@ class TodoItem < ApplicationRecord
   validates_presence_of :content
   before_save :trim
 
-  def textarea_rows
-    (content.length.to_f / 60).ceil
-  end
-
   # Max one line break between lines, one space between words, and no whitespace around
   def trim
     self.content = self.content.squeeze("\n").squeeze(" ").strip
