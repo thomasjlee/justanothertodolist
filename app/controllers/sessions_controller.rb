c9de9a94d5b935b58ac937814d81535348637ba4
Thomas Lee <leethomasjae@gmail.com>
2019-02-26 11:33:07 -0600

    Add sign out form to main layout.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,11 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path
+  end
+
   protected
 
   def auth_hash
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/auth/:provider/callback", to: "sessions#create"
+  delete "/sign_out", to: "sessions#destroy"
 
   root to: "lists#index"
 
